3225e92c5ed7a7546e972ef00ea57150ba12305f
guest271314 <guest271314@gmail.com>
2025-06-27 05:23:04 +0000

    Update direct-socket-controller.js

--- direct-sockets/direct-socket-controller.js
+++ direct-sockets/direct-socket-controller.js
@@ -2,45 +2,45 @@ var encoder = new TextEncoder();
 var local = new RTCPeerConnection({
   sdpSemantics: "unified-plan",
 });
-["onsignalingstatechange", "oniceconnectionstatechange", "onicegatheringstatechange", ].forEach( (e) => local.addEventListener(e, console.log));
+[
+  "onsignalingstatechange",
+  "oniceconnectionstatechange",
+  "onicegatheringstatechange",
+].forEach((e) => local.addEventListener(e, console.log));
 
 local.onicecandidate = async (e) => {
   if (!e.candidate) {
     try {
       if (globalThis?.openIsolatedWebApp) {
-        await openIsolatedWebApp(`?name=Signed Web Bundle in Isolated Web App`);
+        await openIsolatedWebApp(`?name=TCPSocket`);
       } else {
-        setTitle(`?=Signed Web Bundle in Isolated Web App`);
+        setTitle(`?=TCPSocket`);
       }
-      await scheduler.postTask( () => {}
-      , {
+      await scheduler.postTask(() => {}, {
         delay: 2200,
-        priority: "user-visible"
+        priority: "user-visible",
       });
       console.log("sdp:", local.localDescription);
       var abortable = new AbortController();
-      var {signal} = abortable;
+      var { signal } = abortable;
       var request = await fetch("http://0.0.0.0:44819", {
         method: "post",
         body: new TextEncoder().encode(local.localDescription.sdp),
         signal,
-      }).then( (r) => r.text()).then(async (text) => {
+      }).then((r) => r.text()).then(async (text) => {
         await local.setRemoteDescription({
           type: "answer",
           sdp: text,
         });
         console.log("Done signaling SDP");
-      }
-      ).catch( (e) => {
+      }).catch((e) => {
         console.log(e);
-      }
-      );
+      });
     } catch (e) {
       console.error(e);
     }
   }
-}
-;
+};
 var channel = local.createDataChannel("transfer", {
   negotiated: true,
   ordered: true,
@@ -51,16 +51,13 @@ var channel = local.createDataChannel("transfer", {
 
 channel.onopen = async (e) => {
   console.log(e.type, e.target);
-}
-;
+};
 channel.onclose = async (e) => {
   console.log(e.type, e.target);
-}
-;
+};
 channel.onclosing = async (e) => {
   console.log(e.type);
-}
-;
+};
 
 channel.onerror = async (e) => {
   console.log(e.type, e.target);
@@ -69,8 +66,7 @@ channel.onerror = async (e) => {
 channel.onmessage = async (e) => {
   // Do stuff with data
   console.log(e.data);
-}
-;
+};
 
 var offer = await local.createOffer({
   voiceActivityDetection: false,
