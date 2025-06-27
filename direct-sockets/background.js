a79a1964b1cabc875c5985c7df95b78e1ba54329
guest271314 <guest271314@gmail.com>
2025-06-27 03:59:59 +0000

    Update background.js

--- direct-sockets/background.js
+++ direct-sockets/background.js
@@ -1,25 +1,26 @@
 globalThis.name = chrome.runtime.getManifest().short_name;
-globalThis.port;
+globalThis.port = null;
 chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
-  if (tab.title === "TCPSocket") {
-    console.log(tab);
-    const[{ id }] = await chrome.windows.getAll({
-      populate: true,
-      windowTypes: ["app"],
-    });
-    await chrome.windows.update(id, {focused: false});
+  if (changeInfo?.title === "TCPSocket" && globalThis.port === null) {
+    console.log(changeInfo, tab, chrome.runtime.getManifest().host_permissions);
     globalThis.port = chrome.runtime.connectNative(globalThis.name);
     port.onMessage.addListener((message) => console.log(message));
     port.onDisconnect.addListener(() => console.log(chrome.runtime.lastError));
+    const [{ id }] = await chrome.windows.getAll({
+      populate: true,
+      windowTypes: ["app"],
+    });
+    await chrome.windows.update(id, { focused: false });
   }
 });
 
 chrome.windows.onCreated.addListener(async ({ type, id }) => {
-  const [{title, url}] = await chrome.tabs.query({windowId:id});
+  const [{ title, url }] = await chrome.tabs.query({ windowId: id });
   console.log("Isolated App open", { type, id, title, url });
 }, { windowTypes: ["app"] });
 
 chrome.windows.onRemoved.addListener((id) => {
   console.log("Isolated App closed", id);
   globalThis?.port?.disconnect();
+  globalThis.port = null;
 }, { windowTypes: ["app"] });
